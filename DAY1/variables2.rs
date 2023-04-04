1d5504a620f5b6ac5a51a653f7b384c34c92f27b
SlimTux <117015097+SlimTux@users.noreply.github.com>
2023-04-04 02:15:52 +0100

    Add files via upload

---
+++ DAY1/variables2.rs
@@ -0,0 +1,11 @@
+// variables2.rs
+// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
+
+fn main() {
+    let x = 10;
+    if x == 10 {
+        println!("x é dez!(10)");
+    } else {
+        println!("x na~o é dez!(≠10)");
+    }
+}
